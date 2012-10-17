e08a92004abae76e111eb9c72ce40038b8c3bec5
Louka Desroziers <ldesroziers@Loukas-iMac.local>
2012-10-17 19:17:46 +0200

    GitHub for Mac: Throw-away commit.

---
+++ PAGradientColorSample/PAGradientSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PAGradientSample
+//
+//  Created by Louka Desroziers on 17/10/12.
+//  Copyright (c) 2012 Louka Desroziers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ PAGradientColorSample/PAGradientSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PAGradientSample
+//
+//  Created by Louka Desroziers on 17/10/12.
+//  Copyright (c) 2012 Louka Desroziers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
